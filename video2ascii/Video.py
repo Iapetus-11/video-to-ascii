cd8df8b4e72369f8234cd8ad07aa1b95cb3c5ed1
Iapetus11 <olimdahacker@gmail.com>
2020-08-23 18:26:51 -0400

    add FileNotFound error

--- video2ascii/Video.py
+++ video2ascii/Video.py
@@ -1,4 +1,13 @@
 import ffmpeg
+import os
+
+
+class FileNotFound(Exception):
+    def __init__(self, file: str, msg: str = 'File \'{0}\' not found!'):
+        self.msg = msg.format(file)
+
+    def __str__(self):
+        return self.msg
 
 
 class Video:
@@ -7,5 +16,11 @@ class Video:
         self.h = h
 
         self.file_name = file_name
+
+        if not os.isfile(self.file_name):
+            raise FileNotFound(self.file_name)
+
+        if not os.isfile('ffmpeg.exe'):
+
 
         self.video_input = ffmpeg.input(file_name)
