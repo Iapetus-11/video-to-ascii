9d63595b6a342e9f92c081febd8a7588681eb6c0
Iapetus11 <olimdahacker@gmail.com>
2020-08-23 17:43:52 -0400

    add Video.py

---
+++ video2ascii/Video.py
@@ -0,0 +1,11 @@
+import ffmpeg
+
+
+class Video:
+    def __init__(*, w: int, h: int, file_name: str):
+        self.w = w
+        self.h = h
+
+        self.file_name = file_name
+
+        self.video_input = ffmpeg.input(file_name)
