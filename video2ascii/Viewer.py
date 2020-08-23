543fd84dd2b4121396401e75e9ed5eda023b5c6a
Iapetus11 <olimdahacker@gmail.com>
2020-08-23 19:18:05 -0400

    add view frame function to Viewer.py

--- video2ascii/Viewer.py
+++ video2ascii/Viewer.py
@@ -8,6 +8,15 @@ class Viewer:
         for attr in meta.keys():
             self.__dict__[attr] = meta[attr]
 
+    def view_frame(f_index: int):
+        frame = frames[f_index]
+        body = ''
+
+        for row in frame:
+            body += '\n' + ''.join(row)
+
+        print(body)
+
     def _view_frame(frame):
         os.system('cls')
 
